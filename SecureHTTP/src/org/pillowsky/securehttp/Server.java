6973c991e9e55795533b06f234b8a3cf18f9adf1
Yuzo <hyzgog@gmail.com>
2015-06-04 23:16:20 +0800

    basic jump done

--- SecureHTTP/src/org/pillowsky/securehttp/Server.java
+++ SecureHTTP/src/org/pillowsky/securehttp/Server.java
@@ -2,6 +2,7 @@ package org.pillowsky.securehttp;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.PrintStream;
 import java.io.PrintWriter;
 import java.io.IOException;
 import java.net.InetAddress;
@@ -54,32 +55,30 @@ public class Server implements Runnable {
                 BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
                 PrintWriter out = new PrintWriter(socket.getOutputStream());
 
+                String response;
                 String line = in.readLine();
                 String[] words = line.split(" ");
-
-                String response;
                 if ("GET".equals(words[0]) && "/".equals(words[1])) {
                 	response = local();
                 } else {
-                	if ("POST".equals(words[0]) && "/remote".equals(words[1])) {
-                		response = remote();
-                	} else {
-                		response = invalid();
+                	if ("POST".equals(words[0])) {
+                		switch(words[1]) {
+	                		case "/remote":
+	                			response = remote();
+	                			break;
+	                		case "/portal":
+	                			response = portal();
+	                			break;
+	                		default:
+	                			response = notFound();
+                		}
+             		} else {
+                		response = notFound();
                 	}
                 }
-                
-                out.println("HTTP/1.1 200 OK");
-                out.println("Server: SecureHTTP");
-                out.println("Connection: close");
-                out.println("Content-Type: text/html; charset=UTF-8");
-                out.println(String.format("Content-Length: %d", response.length()));
-                out.println();
 
-                out.println(response);
-                out.println();
+                out.print(response);
                 out.flush();
-
-                in.close();
                 out.close();
                 socket.close();
 
@@ -88,17 +87,59 @@ public class Server implements Runnable {
                 e.printStackTrace();
             }
         }
-        
+
         private String local() {
-        	return String.format("<form action='/remote' method='post'><p>Local Page on %s:%d</p><input type='submit' value='Visit Remote Page' /></form>", localAddr, localPort);
+        	String body = String.format("<form action='/remote' method='post'><p>Local Page on %s:%d</p><input type='submit' value='Visit Remote Page' /></form>", localAddr, localPort);
+        	return buildResponse(body, 200, "OK");
         }
         
         private String remote() {
-        	return String.format("<form action='/' method='get'><p>Remote Page on %s:%d</p><input type='submit' value='Visit Local Page' /></form>", localAddr, localPort);
+        	try {
+        		Socket client = new Socket(remoteAddr, remotePort);
+
+                PrintStream out = new PrintStream(client.getOutputStream());
+                BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+
+                out.println("POST /portal HTTP/1.1");
+                out.println();
+
+                StringBuilder body = new StringBuilder();
+                String line;
+                while ((line = in.readLine()) != null) {
+                	body.append(line);
+                	body.append("\n");
+                }
+                body.append("\n\n");
+
+                out.flush();
+                out.close();
+                client.close();
+            	return body.toString();
+        	} catch(IOException e) {
+                e.printStackTrace();
+                return e.toString();
+            }
         }
         
-        private String invalid() {
-        	return "INVALID";
+        private String portal() {
+        	String body = String.format("<form action='/'><p>Remote Page on %s:%d</p><input type='submit' value='Visit Local Page' /></form>", localAddr, localPort);
+        	return buildResponse(body, 200, "OK");
+        }
+
+        private String notFound() {
+        	return buildResponse("404 Not Found", 404, "Not Found");
+        }
+        
+        private String buildResponse(String body, int status, String textStatus) {
+        	StringBuilder response = new StringBuilder();
+        	response.append(String.format("HTTP/1.1 %d %s%n", status, textStatus));
+        	response.append("Server: SecureHTTP\n");
+        	response.append("Connection: close\n");
+        	response.append("Content-Type: text/html; charset=UTF-8\n");
+        	response.append(String.format("Content-Length: %d%n%n", body.length()));
+        	response.append(body);
+        	response.append("\n\n");
+        	return response.toString();
         }
     }
 }
